3d1dcdaf97f4895084295078e5d1febc535b624f
Lisa Kraisriwatana <humanwave11@gmail.com>
2023-06-13 10:10:33 -0400

    fixed about picture size issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "742bdaa63046894b"; }
+/******/ 		__webpack_require__.h = function() { return "7c9ae082a87f5f95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
